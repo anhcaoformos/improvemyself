d2ce004b621a67be32d879290388ee64b023f813
anhcaoformos <139545964+anhcaoformos@users.noreply.github.com>
2024-03-21 22:00:46 +0700

    Initial version of improvemyself generated by generator-jhipster@8.2.1

---
+++ src/main/java/com/produck/aop/logging/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Logging aspect.
+ */
+package com.produck.aop.logging;
---
+++ src/test/java/com/produck/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.produck;
+
+import com.produck.config.AsyncSyncConfiguration;
+import com.produck.config.EmbeddedSQL;
+import com.produck.config.JacksonConfiguration;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { ImprovemyselfApp.class, JacksonConfiguration.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
